079b42d7f2f54e9c293d9d578f47011725455f33
seniorWebDev929 <fullstackdev929@hotmail.com>
2020-04-14 22:01:58 +0200

    update title

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link type="text/css" rel="stylesheet" href="./assets/css/style.css" />
-    <title>Share Point</title>
+    <title>SharePoint</title>
   </head>
   <body>
     <div id="chooseFormat">
